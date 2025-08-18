6ea7cce406fdd81e39109dbe249e078c026220ab
Blue <imnotbluefrfr@gmail.com>
2025-08-17 20:42:33 -0500

    Update load.js

--- public/js/load.js
+++ public/js/load.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(window.location.search)
 if (params.get("game")) {
     games.forEach(async game => {
         if (game.id != params.get("game")) return
-        document.title = `${game.title} | LeButt Crack`
+        document.title = `${game.title} | LeBron James Game's`
         document.querySelector("#gameImage").src = game.image
         document.querySelector("#gameTitle").innerHTML = game.title
         if (game.description) document.querySelector("#gameDescription").innerHTML = game.description
